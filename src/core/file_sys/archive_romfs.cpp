2815f9668c98eafce504b4223ab9c48b6651903e
condut <g6689136@trbvm.com>
2015-07-10 01:02:27 +0300

    .

--- src/core/file_sys/archive_romfs.cpp
+++ src/core/file_sys/archive_romfs.cpp
@@ -18,7 +18,7 @@ namespace FileSys {
 
 ArchiveFactory_RomFS::ArchiveFactory_RomFS(const Loader::AppLoader& app_loader) {
     // Load the RomFS from the app
-    if (Loader::ResultStatus::Success != app_loader.ReadRomFS (m_romfs_file, m_offset, m_size)) {
+    if (Loader::ResultStatus::Success != app_loader.ReadRomFS(m_romfs_file, m_offset, m_size)) {
         LOG_ERROR(Service_FS, "Unable to read RomFS!");
     }
 }
